fb33229df6346402552f4bcdb2718b724e3e1581
nir <niritzhak10@gmail.com>
2020-12-04 12:40:27 +0200

    Finish the HiberanteDAO and create the UsersDAO

--- src/main/java/org/nir/bookstore/dao/HibernateDAO.java
+++ src/main/java/org/nir/bookstore/dao/HibernateDAO.java
@@ -1,7 +1,26 @@
 package org.nir.bookstore.dao;
 
-public class HibernateDAO 
-{
-	
+import org.hibernate.Session;
 
+public class HibernateDAO<T>
+{
+	protected Session session ;
+
+	public HibernateDAO(Session session) 
+	{
+		super();
+		this.session = session;
+	} 
+	
+	
+	public T create(T t)
+	{
+		session.getTransaction().begin();
+		session.save(t);
+		session.flush();
+		session.refresh(t);
+		
+		session.getTransaction().commit();
+		return t; 
+	}
 }
---
+++ src/main/java/org/nir/bookstore/dao/UsersDAO.java
@@ -0,0 +1,7 @@
+package org.nir.bookstore.dao;
+
+public class UsersDAO extends HibernateDAO<T> implements GenericeDAO<T> 
+{
+	
+
+}
