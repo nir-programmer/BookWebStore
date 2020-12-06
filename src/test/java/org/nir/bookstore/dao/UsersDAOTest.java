cf2a8a7ba4fab0867da89533f30ba4d2c6214d4b
nir <niritzhak10@gmail.com>
2020-12-06 14:53:18 +0200

    Create a Junit 4 test case

---
+++ src/test/java/org/nir/bookstore/dao/UsersDAOTest.java
@@ -0,0 +1,14 @@
+package org.nir.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsersDAOTest {
+
+	@Test
+	public void testCreateUsers() {
+		fail("Not yet implemented");
+	}
+
+}
