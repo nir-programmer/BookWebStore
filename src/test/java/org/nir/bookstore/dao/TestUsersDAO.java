8e2240f3852eb3bc6d79c8c2c3a61731849589d8
nir <niritzhak10@gmail.com>
2020-12-07 09:24:33 +0200

    Adding BeforeAll and AfterAll methods

--- src/test/java/org/nir/bookstore/dao/TestUsersDAO.java
+++ src/test/java/org/nir/bookstore/dao/TestUsersDAO.java
@@ -11,12 +11,45 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.nir.bookstore.entities.Users;
 
-public class TestUsersDAO {
+public class TestUsersDAO 
+{
+	private static SessionFactory sessionFactory; 
+	private static Session session ; 
+	private static UsersDAO usersDAO; 
+	
+	
+	@BeforeAll
+	@DisplayName("when create SessionFactory , Session , UsersDAO object")
+	public static void init()
+	{
+		sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Users.class)
+				.buildSessionFactory();
 
+		 session = sessionFactory.getCurrentSession();
+		 
+		 usersDAO = new UsersDAO(session); 
+		 
+	}
+	
+	@AfterAll
+	@DisplayName("when colose the SessionFactroy and Session")
+	public static void tearDown()
+	{
+		session.close();
+		sessionFactory.close();
+	}
+	
+	
+	
+	
 	@Test
 	public void testCreateUsers() {
 		Users user1 = new Users();
