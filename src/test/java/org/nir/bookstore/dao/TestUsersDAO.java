636cdae40f366e050762ee5858a01a6504e73686
nir <niritzhak10@gmail.com>
2020-12-07 09:13:04 +0200

    ok

--- src/test/java/org/nir/bookstore/dao/TestUsersDAO.java
+++ src/test/java/org/nir/bookstore/dao/TestUsersDAO.java
@@ -1,76 +1,74 @@
 package org.nir.bookstore.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.hibernate.PropertyValueException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.*;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.nir.bookstore.entities.Users;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class TestUsersDAO {
-	private static SessionFactory sessionFactory;
-	private static Session session;
-	private static UsersDAO usersDAO;
 
-	@BeforeAll
-	@DisplayName("when try to create a session factory")
-	public static void init() {
-		sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Users.class)
-				.buildSessionFactory();
+	@Test
+	public void testCreateUsers() {
+		Users user1 = new Users();
+		user1.setEmail("niritziahk10@gmial.com");
+		user1.setFullName("Nir Ithzak");
+		user1.setPassword("power");
 
-		
-		  session = sessionFactory.getCurrentSession();
-		  
-		  usersDAO = new UsersDAO(session);
-		  System.out.println("\n***************************************************");
-		
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Users.class).buildSessionFactory();
+
+		Session session = sessionFactory.getCurrentSession();
+
+		session.getTransaction().begin();
+
+		session.persist(user1);
+
+		session.getTransaction().commit();
+
+		session.close();
+		sessionFactory.close();
+
+		assertTrue(user1.getUserId() > 0);
+
 	}
-	
-	
-	
-	
-	@AfterAll
-	@DisplayName("when try to close SessionFactory and Session objects")
-	public static void tearDown() {
-		if (session != null)
-			session.close();
-		if (sessionFactory != null)
-			sessionFactory.close();
-	}
-	
-	////////////////////////////////////////////////////
-	
+
 	@Test
 	@Disabled
-	@DisplayName("testGetFoundUser")
-	void testGetFoundUser()
+	void testCreateUsersFieldsNotSet() 
 	{
-		Integer id = 1; 
-		Users users = usersDAO.get(id);
+		Users user1 = new Users();
+		/*
+		 * user1.setEmail("niritziahk10@gmial.com"); user1.setFullName("Nir Ithzak");
+		 * user1.setPassword("power");
+		 */
 		
-		System.out.println("testGetFoundSuser(): user with id = " + id);
-		System.out.println(users);
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Users.class)
+				.buildSessionFactory();
 		
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.getTransaction().begin();
+		
+		session.persist(user1);
+		
+		session.getTransaction().commit();
+		
+		session.close();
+		sessionFactory.close();
+		
+		
+
 	}
-	
-	@Test
-	void testDeleteFoundUsers()
-	{
-		Integer id = 1; 
-		
-		usersDAO.delete(id);
-		
-		
-	}
+
 }
