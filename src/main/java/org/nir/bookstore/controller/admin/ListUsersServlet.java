77d5db56ae1fd2ccefef21650bb6a9c9003ee1fc
nir <niritzhak10@gmail.com>
2020-12-07 12:00:34 +0200

    Creatint the ListUsersSevlet and UsersService classes

---
+++ src/main/java/org/nir/bookstore/controller/admin/ListUsersServlet.java
@@ -0,0 +1,33 @@
+package org.nir.bookstore.controller.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListUsersServlet
+ */
+@WebServlet("/admin/list_users")
+public class ListUsersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ListUsersServlet() 
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+}
---
+++ src/main/java/org/nir/bookstore/service/UsersService.java
@@ -0,0 +1,7 @@
+package org.nir.bookstore.service;
+
+public class UsersService 
+{
+	
+
+}
