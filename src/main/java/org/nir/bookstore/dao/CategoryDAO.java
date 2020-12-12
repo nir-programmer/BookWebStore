00e2e28b59a59b2bf6f1c3500e49d3d2053aaa08
nir <niritzhak10@gmail.com>
2020-12-12 16:55:30 +0200

    Create the CategoryDAO class

---
+++ src/main/java/org/nir/bookstore/dao/CategoryDAO.java
@@ -0,0 +1,45 @@
+package org.nir.bookstore.dao;
+
+import java.util.List;
+
+import org.nir.bookstore.entities.Category;
+
+public class CategoryDAO extends HibernateDAO<Category> implements GenericeDAO<Category> {
+
+	@Override
+	public Category create(Category category) 
+	{
+		return super.create(category);
+	}
+
+	@Override
+	public Category update(Category t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Category get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Category> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
