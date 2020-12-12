7063fa022c22286c8d4e59f015622179e13d0c18
nir <niritzhak10@gmail.com>
2020-12-12 16:07:42 +0200

    TestCategory.testCrateCategory() works!

--- src/test/java/org/nir/bookstore/entities/TestCategory.java
+++ src/test/java/org/nir/bookstore/entities/TestCategory.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.junit.jupiter.api.AfterAll;
@@ -24,112 +25,49 @@ import org.nir.bookstore.entities.Users;
 
 public class TestCategory 
 {
-	private static SessionFactory sessionFactory ; 
-	private static Session session; 
+	private static SessionFactory sessionFactory; 
+	
 	@BeforeAll
-	@DisplayName("when create session factory")
-	static void init()
+	public static void init()
 	{
-		System.out.println(">>init():try to create session factory..");
-		sessionFactory = new Configuration().configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Book.class)
-				.addAnnotatedClass(BookOrder.class)
-				.addAnnotatedClass(Category.class)
-				.addAnnotatedClass(OrderDetail.class)
-				.addAnnotatedClass(OrderDetailId.class)
-				.addAnnotatedClass(Review.class)
-				.addAnnotatedClass(Users.class)
-				.addAnnotatedClass(Customer.class)
-				.buildSessionFactory();
-		
-	
+		System.out.println(">>init():try to create SessionFactory...");
+		 sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				 .addAnnotatedClass(Category.class)
+				 .addAnnotatedClass(Book.class)
+				 .addAnnotatedClass(Customer.class)
+				 .addAnnotatedClass(OrderDetailId.class)
+				 .addAnnotatedClass(OrderDetail.class)
+				 .addAnnotatedClass(Review.class)
+				 .addAnnotatedClass(Users.class)
+				 .addAnnotatedClass(BookOrder.class)
+				 .buildSessionFactory();
+		 
+		 System.out.println(">>init():SessionFactory Created!");
 	}
 	
-	@AfterAll
-	@DisplayName("when try close the session factory")
-	static void tearDown()
-	{
-		if(sessionFactory != null)
-		{
-		System.out.println(">>tearDown():try to close the session factory..."); 
-		sessionFactory.close();
-		System.out.println(">>teadDown():session factory  closed!"); 
-		System.out.println("\n***************************************************");
-		}
+	@Test
+	@DisplayName("when create a category")
+	void testCreateCategory()
+	{ 
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		Category category = new Category("Science Fiction"); 
+		System.out.println(">>testCreateCategory():try to save the category...");
+		
+		session.save(category);
 		
 		
-	}
-	@BeforeEach
-	@DisplayName(">>createSession():try to create a new Session")
-	void createSession()
-	{
-		System.out.println(">>createSession():try to create a new Session and transaction"); 
-		session = sessionFactory.getCurrentSession();
-		session.getTransaction();
-		
-		session.beginTransaction();
-		System.out.println(">>createSession():Session Created!");
-		
-		
-		
-		
-		System.out.println("\n***************************************************");
-
-	}
-	
-	@AfterEach
-	@DisplayName("when try close the session")
-	void closeSession()
-	{
-		if(session != null)
-		{
-			System.out.println(">>closeSession():try to commit transaction..."); 
-		session.getTransaction().commit();
-		System.out.println(">>closeSession():transaction commited!"); 
-		
-		System.out.println(">>closeSession():try to close the session..."); 
+		transaction.commit();
 		session.close();
-		System.out.println(">>closeSession():session closed!"); 
-		System.out.println("\n***************************************************");
-
-		}
+		System.out.println(">>testCreateCategory():Category Persisted!");
+		
+		/*
+		org.hibernate.AnnotationException: 
+		@OneToOne or @ManyToOne on org.nir.bookstore.entities.OrderDetail.bookOrder references an unknown entity:
+		 org.nir.bookstore.entities.BookOrder
+		*/
 		
 		
 	}
-	
-	
-	/*
-	 * **********************************TESTS***********************************
-	 * */
-	
-	
-	@Test
-	//@Disabled
-	@DisplayName("when trying to add a new category to db")
-	void testAddNewCategory()
-	{
-			System.out.println(">>testAddNewCategory():try to add 2 categories : Comix and Science");
-			Category category1 = new Category("Comix");
-			Category category2 = new Category("Science");
-			
-			session.save(category1);
-			session.save(category2);
-			
-			
-			System.out.println(">>testAddNewCategory():Comix and Science presisted!!!");
-	}
-	
-	@Test
-	//@Disabled
-	@DisplayName("when reading categories")
-	void testGetAllCategories() 
-	{
-		
-		Query<Category> query = session.createQuery("FROM Category");
-		List<Category> categories = query.getResultList();
-		System.out.println(">>testGetAllCategories(): categories:");
-		System.out.println(categories);
-		
-	}
-
 }
