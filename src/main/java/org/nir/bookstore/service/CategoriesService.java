92e7ce087bca0f28d5db62a0f08dae01ab6cf8d3
nir <niritzhak10@gmail.com>
2020-12-14 10:05:54 +0200

    CATEGORY: EDIT FEATURE WORKS!

--- src/main/java/org/nir/bookstore/service/CategoriesService.java
+++ src/main/java/org/nir/bookstore/service/CategoriesService.java
@@ -112,8 +112,16 @@ public class CategoriesService extends BaseService
 		Category categoryById = categoryDAO.get(id); 
 		Category categoryByName = categoryDAO.findByName(name); 
 		
-		if(categoryByName == null || (categoryByName != null &&
-			categoryById.getCategoryId() == categoryByName.getCategoryId()))
+		
+		if(categoryByName != null && (categoryById.getCategoryId() != categoryByName.getCategoryId()))
+		{
+			categoryDAO.closeCurrentSessionWithTransaction();
+			String message = "could not update category. "
+			+ "Category with name : " + name + " Exists!"; 
+			request.setAttribute("message", message);
+			request.getRequestDispatcher("message.jsp").forward(request, response);
+		}
+		else
 		{
 			Category category = new Category(name); 
 			categoryDAO.update(category);
@@ -121,118 +129,10 @@ public class CategoriesService extends BaseService
 			listAll("Category updated seccussfully");
 		}
 		
-		else
-		{
-			categoryDAO.closeCurrentSessionWithTransaction();
-			String message = "could not update category. +"
-					+ "Category with name : " + name + " Exists!"; 
-			request.setAttribute("message", message);
-		}
-		
 		
 		
 	}
-	/*
-	 * // OK public void getAllUsers(String message) throws ServletException,
-	 * IOException { if (message != null) request.setAttribute("message", message);
-	 * 
-	 * usersDAO.openCurrentSession(); List<Users> users = usersDAO.listAll();
-	 * usersDAO.closeCurrentSession();
-	 * 
-	 * request.setAttribute("users", users);
-	 * request.getRequestDispatcher("users_list.jsp").forward(request, response);
-	 * 
-	 * }
-	 * 
-	 * // OK public void getAllUsers() throws ServletException, IOException {
-	 * getAllUsers(null); }
-	 * 
-	 * // OK public void createUser() throws ServletException, IOException { String
-	 * email = request.getParameter("email"); String fullName =
-	 * request.getParameter("fullname"); String password =
-	 * request.getParameter("password");
-	 * 
-	 * usersDAO.openCurrentSession(); Users existUser = usersDAO.findByEmail(email);
-	 * usersDAO.closeCurrentSession();
-	 * 
-	 * if (existUser != null) {
-	 * System.out.println(">>UsersService.createUser():The User " + existUser +
-	 * " Exists!"); String message = "Couldn't create a user. " +
-	 * "A user with email " + email + " already exists!";
-	 * request.setAttribute("message", message);
-	 * request.getRequestDispatcher("message.jsp").forward(request, response); }
-	 * else { Users newUser = new Users(email, password, fullName);
-	 * 
-	 * usersDAO.openCurrentSessionWithTransaction(); usersDAO.create(newUser);
-	 * usersDAO.closeCurrentSessionWithTransaction();
-	 * getAllUsers("User created succssfully!"); }
-	 * 
-	 * }
-	 * 
-	 * public void editUser() throws ServletException, IOException { int userId =
-	 * Integer.parseInt(request.getParameter("id"));
-	 * 
-	 * usersDAO.openCurrentSession(); Users user = usersDAO.get(userId);
-	 * usersDAO.closeCurrentSession();
-	 * 
-	 * // System.out.println("THE User is: " + user.getFullName());
-	 * 
-	 * request.setAttribute("user", user);
-	 * 
-	 * request.getRequestDispatcher("users_form.jsp").forward(request, response);
-	 * 
-	 * }
-	 * 
-	 * public void updateUser() throws ServletException, IOException { int id =
-	 * Integer.parseInt(request.getParameter("userId")); String email =
-	 * request.getParameter("email"); String fullName =
-	 * request.getParameter("fullname"); String password =
-	 * request.getParameter("password");
-	 * 
-	 * System.out.println(">>UsersService.update()" + "\n" + id + " , " + email +
-	 * " , " + fullName + " , " + password);
-	 * 
-	 * Users user = new Users(id, email, password, fullName);
-	 * 
-	 * usersDAO.openCurrentSessionWithTransaction(); Users emailUser =
-	 * usersDAO.findByEmail(user.getEmail()); Users idUser =
-	 * usersDAO.get(user.getUserId());
-	 * 
-	 * 
-	 * case 1: the email does not exists(normal case) OR the email exists - but it
-	 * belongs to the current User: update the current user including email
-	 * 
-	 * if (emailUser == null || (emailUser != null) && (emailUser.getUserId() ==
-	 * idUser.getUserId())) { usersDAO.update(user);
-	 * usersDAO.closeCurrentSessionWithTransaction();
-	 * getAllUsers("User updated successfully"); } else {
-	 * usersDAO.closeCurrentSessionWithTransaction(); String message =
-	 * "Could not update user. User with email " + user.getEmail() +
-	 * " already exists"; request.setAttribute("message", message);
-	 * request.getRequestDispatcher("message.jsp").forward(request, response); }
-	 * 
-	 * }
-	 * 
-	 * public void deleteUser() throws ServletException, IOException { Integer
-	 * userID = Integer.parseInt(request.getParameter("id"));
-	 * 
-	 * // System.out.println(">>UserService.deleteUser(): id = " + userID);
-	 * 
-	 * usersDAO.openCurrentSessionWithTransaction(); usersDAO.delete(userID);
-	 * System.out.println(">>UsersService.delteUser:User with id = " + userID +
-	 * " Dleted"); usersDAO.closeCurrentSessionWithTransaction();
-	 * 
-	 * getAllUsers("User Deleted Succussfully");
-	 * 
-	 * }
-	 */
-
 	
-
-	
-
-	
-
 	/******************************************************
 	 * METHODS USED FOR TESTS
 	 ******************************************************
