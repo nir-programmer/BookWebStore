8be61a853e79dc4bde32fccc282b87f93d710769
nir <niritzhak10@gmail.com>
2020-12-16 12:22:38 +0200

    Create the BookDAO class with delegating calls for implemenatins

--- src/main/java/org/nir/bookstore/dao/BookDAO.java
+++ src/main/java/org/nir/bookstore/dao/BookDAO.java
@@ -1,5 +1,49 @@
 package org.nir.bookstore.dao;
 
-public class BookDAO extends HibernateDAO<E> implements GenericeDAO<T> {
+import java.util.List;
 
+import org.nir.bookstore.entities.Book;
+
+public class BookDAO extends HibernateDAO<Book> implements GenericeDAO<Book> 
+{
+	@Override
+	public Book create(Book book)
+	{
+		return super.create(book); 
+	}
+
+	@Override
+	public Book update(Book book)
+	{
+		return super.update(book);
+	}
+
+	@Override
+	public Book get(Object id)
+	{
+		return super.find(Book.class, id); 
+	}
+
+	@Override
+	public void delete(Object id) 
+	{
+		
+		
+	}
+
+	@Override
+	public List<Book> listAll() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+	
 }
