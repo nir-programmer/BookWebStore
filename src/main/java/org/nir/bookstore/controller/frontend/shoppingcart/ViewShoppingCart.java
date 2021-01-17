d17d08cfb5aa7d278fc43976b8fc42ed1f7e3783
nir <niritzhak10@gmail.com>
2021-01-17 10:52:34 +0200

    Creates ViewCartServlet and shopping_cart.jsp

---
+++ src/main/java/org/nir/bookstore/controller/frontend/shoppingcart/ViewShoppingCart.java
@@ -0,0 +1,28 @@
+package org.nir.bookstore.controller.frontend.shoppingcart;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewShoppingCart
+ */
+@WebServlet("/view_cart")
+public class ViewShoppingCart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ViewShoppingCart() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
