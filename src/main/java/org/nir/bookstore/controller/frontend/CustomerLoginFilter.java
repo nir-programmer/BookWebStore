9c152ec189030a57b190e1d25a34d7b36808936b
nir <niritzhak10@gmail.com>
2021-01-29 11:01:17 +0200

    Finish PlaceOrderupdate filter to includes URL : "/checkou" and "/place_order"

--- src/main/java/org/nir/bookstore/controller/frontend/CustomerLoginFilter.java
+++ src/main/java/org/nir/bookstore/controller/frontend/CustomerLoginFilter.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpSession;
 public class CustomerLoginFilter implements Filter
 {
 	private static final String[] loginRequiredURLs = { "/view_profile", "/edit_profile", "/update_profile",
-			"/write_review" };
+			"/write_review" , "/checkout" , "/place_order"};
 
 	public CustomerLoginFilter()
 	{
