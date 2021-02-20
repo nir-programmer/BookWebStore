6ae763528eaa036c5c9d6c13fa999dd5f2da58dc
nir <niritzhak10@gmail.com>
2021-02-20 12:12:15 +0200

    Create the Article entity class

---
+++ src/main/java/org/nir/bookstore/entities/Article.java
@@ -0,0 +1,64 @@
+package org.nir.bookstore.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "articles")
+public class Article
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "article_id")
+	private Integer articleId; 
+	
+	private String title ; 
+	
+	private String content;
+
+	public Article() {}
+	
+	public Article(String title, String content)
+	{
+		super();
+		this.title = title;
+		this.content = content;
+	}
+
+	public Integer getArticleId()
+	{
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId)
+	{
+		this.articleId = articleId;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public String getContent()
+	{
+		return content;
+	}
+
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+	
+	
+
+}
